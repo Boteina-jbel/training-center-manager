168c4ed4ad220370b056b3f280f719376b244e73
Boteina <boteinajbel@gmail.com>
2026-06-09 00:16:41 +0100

    feature/add-trainer-model

--- src/main/java/org/mql/jee/trainingcenter/models/Trainer.java
+++ src/main/java/org/mql/jee/trainingcenter/models/Trainer.java
@@ -3,42 +3,42 @@ package org.mql.jee.trainingcenter.models;
 import java.sql.Timestamp;
 
 public class Trainer {
-//
-//    private int id;
-//    private String firstName;
-//    private String lastName;
-//    private String email;
-//    private String specialization;
-//    private Timestamp createdAt;
-//
-//    public Trainer() {
-//    }
-//
-//    public Trainer(int id, String firstName, String lastName, String email, String specialization, Timestamp createdAt) {
-//        this.id = id;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//        this.specialization = specialization;
-//        this.createdAt = createdAt;
-//    }
-//
-//    // Getters & Setters
-//    public int getId() { return id; }
-//    public void setId(int id) { this.id = id; }
-//
-//    public String getFirstName() { return firstName; }
-//    public void setFirstName(String firstName) { this.firstName = firstName; }
-//
-//    public String getLastName() { return lastName; }
-//    public void setLastName(String lastName) { this.lastName = lastName; }
-//
-//    public String getEmail() { return email; }
-//    public void setEmail(String email) { this.email = email; }
-//
-//    public String getSpecialization() { return specialization; }
-//    public void setSpecialization(String specialization) { this.specialization = specialization; }
-//
-//    public Timestamp getCreatedAt() { return createdAt; }
-//    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String specialization;
+    private Timestamp createdAt;
+
+    public Trainer() {
+    }
+
+    public Trainer(int id, String firstName, String lastName, String email, String specialization, Timestamp createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.specialization = specialization;
+        this.createdAt = createdAt;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
+
+    public Timestamp getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
 }
