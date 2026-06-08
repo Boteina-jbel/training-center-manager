57564cdcccb2cccd83190d07bc726c71fac18ee4
Boteina <boteinajbel@gmail.com>
2026-06-09 00:14:29 +0100

    save project

---
+++ src/main/java/org/mql/jee/trainingcenter/models/Enrollment.java
@@ -0,0 +1,36 @@
+package org.mql.jee.trainingcenter.models;
+
+import java.sql.Date;
+
+public class Enrollment {
+
+    private int id;
+
+    private Student student;
+    private Training training;
+
+    private Date enrollmentDate;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(int id, Student student, Training training, Date enrollmentDate) {
+        this.id = id;
+        this.student = student;
+        this.training = training;
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public Student getStudent() { return student; }
+    public void setStudent(Student student) { this.student = student; }
+
+    public Training getTraining() { return training; }
+    public void setTraining(Training training) { this.training = training; }
+
+    public Date getEnrollmentDate() { return enrollmentDate; }
+    public void setEnrollmentDate(Date enrollmentDate) { this.enrollmentDate = enrollmentDate; }
+}
