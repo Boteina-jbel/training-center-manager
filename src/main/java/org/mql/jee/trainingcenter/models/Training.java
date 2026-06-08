ed1218b32c4fbc0360f85dc0a36b0bbadf44bef7
Boteina <boteinajbel@gmail.com>
2026-06-09 00:28:20 +0100

    feature/add-training-model

--- src/main/java/org/mql/jee/trainingcenter/models/Training.java
+++ src/main/java/org/mql/jee/trainingcenter/models/Training.java
@@ -3,42 +3,42 @@ package org.mql.jee.trainingcenter.models;
 import java.sql.Timestamp;
 
 public class Training {
-//
-//    private int id;
-//    private String title;
-//    private String description;
-//    private int duration;
-//    private Trainer trainer; // relation object
-//    private Timestamp createdAt;
-//
-//    public Training() {
-//    }
-//
-//    public Training(int id, String title, String description, int duration, Trainer trainer, Timestamp createdAt) {
-//        this.id = id;
-//        this.title = title;
-//        this.description = description;
-//        this.duration = duration;
-//        this.trainer = trainer;
-//        this.createdAt = createdAt;
-//    }
-//
-//    // Getters & Setters
-//    public int getId() { return id; }
-//    public void setId(int id) { this.id = id; }
-//
-//    public String getTitle() { return title; }
-//    public void setTitle(String title) { this.title = title; }
-//
-//    public String getDescription() { return description; }
-//    public void setDescription(String description) { this.description = description; }
-//
-//    public int getDuration() { return duration; }
-//    public void setDuration(int duration) { this.duration = duration; }
-//
-//    public Trainer getTrainer() { return trainer; }
-//    public void setTrainer(Trainer trainer) { this.trainer = trainer; }
-//
-//    public Timestamp getCreatedAt() { return createdAt; }
-//    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
+
+    private int id;
+    private String title;
+    private String description;
+    private int duration;
+    private Trainer trainer; // relation object
+    private Timestamp createdAt;
+
+    public Training() {
+    }
+
+    public Training(int id, String title, String description, int duration, Trainer trainer, Timestamp createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+        this.trainer = trainer;
+        this.createdAt = createdAt;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public int getDuration() { return duration; }
+    public void setDuration(int duration) { this.duration = duration; }
+
+    public Trainer getTrainer() { return trainer; }
+    public void setTrainer(Trainer trainer) { this.trainer = trainer; }
+
+    public Timestamp getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
 }
