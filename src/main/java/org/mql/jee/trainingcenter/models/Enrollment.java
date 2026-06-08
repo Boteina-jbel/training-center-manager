8a92d61b2289fc27356acdc0b3a794faf07f1db9
Boteina <boteinajbel@gmail.com>
2026-06-09 00:26:37 +0100

    feature/add-Enrollment-model

--- src/main/java/org/mql/jee/trainingcenter/models/Enrollment.java
+++ src/main/java/org/mql/jee/trainingcenter/models/Enrollment.java
@@ -4,33 +4,33 @@ import java.sql.Date;
 
 public class Enrollment {
 
-    private int id;
-
-    private Student student;
-    private Training training;
-
-    private Date enrollmentDate;
-
-    public Enrollment() {
-    }
-
-    public Enrollment(int id, Student student, Training training, Date enrollmentDate) {
-        this.id = id;
-        this.student = student;
-        this.training = training;
-        this.enrollmentDate = enrollmentDate;
-    }
-
-    // Getters & Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public Student getStudent() { return student; }
-    public void setStudent(Student student) { this.student = student; }
-
-    public Training getTraining() { return training; }
-    public void setTraining(Training training) { this.training = training; }
-
-    public Date getEnrollmentDate() { return enrollmentDate; }
-    public void setEnrollmentDate(Date enrollmentDate) { this.enrollmentDate = enrollmentDate; }
+//    private int id;
+//
+//    private Student student;
+//    private Training training;
+//
+//    private Date enrollmentDate;
+//
+//    public Enrollment() {
+//    }
+//
+//    public Enrollment(int id, Student student, Training training, Date enrollmentDate) {
+//        this.id = id;
+//        this.student = student;
+//        this.training = training;
+//        this.enrollmentDate = enrollmentDate;
+//    }
+//
+//    // Getters & Setters
+//    public int getId() { return id; }
+//    public void setId(int id) { this.id = id; }
+//
+//    public Student getStudent() { return student; }
+//    public void setStudent(Student student) { this.student = student; }
+//
+//    public Training getTraining() { return training; }
+//    public void setTraining(Training training) { this.training = training; }
+//
+//    public Date getEnrollmentDate() { return enrollmentDate; }
+//    public void setEnrollmentDate(Date enrollmentDate) { this.enrollmentDate = enrollmentDate; }
 }
