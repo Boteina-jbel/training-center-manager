99d3b2993ca4bda5352d91f77ae03d111b52ae48
Boteina <boteinajbel@gmail.com>
2026-06-09 14:34:58 +0100

    feature/add-StudentORM

---
+++ src/main/java/org/mql/jee/trainingcenter/dao/mappers/StudentORM.java
@@ -0,0 +1,33 @@
+package org.mql.jee.trainingcenter.dao.mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mql.jee.trainingcenter.models.Student;
+
+public class StudentORM {
+
+    public static Student getStudent(String[] row) {
+
+        Student s = new Student();
+
+        s.setId(Integer.parseInt(row[0]));
+        s.setFirstName(row[1]);
+        s.setLastName(row[2]);
+        s.setEmail(row[3]);
+        s.setPhone(row[4]);
+
+        return s;
+    }
+
+    public static List<Student> getStudentsList(String[][] data) {
+
+        List<Student> students = new ArrayList<>();
+
+        for (String[] row : data) {
+            students.add(getStudent(row));
+        }
+
+        return students;
+    }
+}
