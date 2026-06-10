8e16c8645b4977a30adcdbecc2aa1602820cdc53
Boteina <boteinajbel@gmail.com>
2026-06-10 01:53:24 +0100

    feature/save-devs

--- src/main/java/org/mql/jee/trainingcenter/dao/mappers/StudentORM.java
+++ src/main/java/org/mql/jee/trainingcenter/dao/mappers/StudentORM.java
@@ -8,28 +8,40 @@ import org.mql.jee.trainingcenter.models.Student;
 
 public class StudentORM {
 
-    public static Student getStudent(String[] row) {
+    public static Student getStudent(String... row) {
 
         Student s = new Student();
 
-        s.setId(Integer.parseInt(row[0]));
+        s.setId(getInt(row[0]));
         s.setFirstName(row[1]);
         s.setLastName(row[2]);
         s.setEmail(row[3]);
         s.setPhone(row[4]);
-        s.setCreatedAt(Timestamp.valueOf(row[5]));
+
+        if(row.length > 5 && row[5] != null) {
+            s.setCreatedAt(Timestamp.valueOf(row[5]));
+        }
 
         return s;
     }
 
     public static List<Student> getStudentsList(String[][] data) {
 
-        List<Student> students = new Vector<>();
+        List<Student> students = new Vector<Student>();
 
-        for (String[] row : data) {
+        for(String[] row : data) {
             students.add(getStudent(row));
         }
 
         return students;
+    }
+
+    public static int getInt(String data) {
+        try {
+            return Integer.parseInt(data);
+        } catch (Exception e) {
+            System.out.println("Mapping Error : " + e.getMessage());
+            return -1;
+        }
     }
 }
