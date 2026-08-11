e6cf918cb2251ee2d0dc2ede71b56aa12160ed3e
Boteina <boteinajbel@gmail.com>
2026-08-11 22:21:06 +0100

    feat: add TrainerException for business validation

---
+++ src/main/java/org/mql/jee/trainingcenter/exceptions/TrainerException.java
@@ -0,0 +1,10 @@
+package org.mql.jee.trainingcenter.exceptions;
+
+public class TrainerException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public TrainerException(String message) {
+        super(message);
+    }
+}
