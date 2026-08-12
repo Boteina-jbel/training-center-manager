8bdadeaccf84162282c4553cb9e1a22732dd4e22
Boteina <boteinajbel@gmail.com>
2026-08-12 22:51:36 +0100

    test: add StudentDao mock for service tests

---
+++ src/main/java/org/mql/jee/trainingcenter/tests/StudentDaoMock.java
@@ -0,0 +1,56 @@
+package org.mql.jee.trainingcenter.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mql.jee.trainingcenter.dao.StudentDao;
+import org.mql.jee.trainingcenter.models.Student;
+
+public class StudentDaoMock implements StudentDao {
+
+    private List<Student> students = new ArrayList<>();
+
+    @Override
+    public List<Student> selectAll() {
+        return new ArrayList<>(students);
+    }
+
+    @Override
+    public Student selectById(int id) {
+
+        for (Student student : students) {
+
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void insert(Student student) {
+        students.add(student);
+    }
+
+    @Override
+    public void update(Student student) {
+
+        for (int i = 0; i < students.size(); i++) {
+
+            if (students.get(i).getId() == student.getId()) {
+
+                students.set(i, student);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+
+        students.removeIf(
+            student -> student.getId() == id
+        );
+    }
+}
