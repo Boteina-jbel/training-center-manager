f3e26c06ffcbc97d49d64e84f2613441503e2eb7
Boteina <boteinajbel@gmail.com>
2026-09-06 14:10:52 +0100

    feat(context): wire Training service and DAO

--- src/main/java/org/mql/jee/trainingcenter/context/ApplicationContext.java
+++ src/main/java/org/mql/jee/trainingcenter/context/ApplicationContext.java
@@ -8,11 +8,15 @@ import org.mql.jee.trainingcenter.business.StudentService;
 import org.mql.jee.trainingcenter.business.StudentServiceDefault;
 import org.mql.jee.trainingcenter.business.TrainerService;
 import org.mql.jee.trainingcenter.business.TrainerServiceDefault;
+import org.mql.jee.trainingcenter.business.TrainingService;
+import org.mql.jee.trainingcenter.business.TrainingServiceDefault;
 
 import org.mql.jee.trainingcenter.dao.StudentDao;
 import org.mql.jee.trainingcenter.dao.StudentDaoJdbc;
 import org.mql.jee.trainingcenter.dao.TrainerDao;
 import org.mql.jee.trainingcenter.dao.TrainerDaoJdbc;
+import org.mql.jee.trainingcenter.dao.TrainingDao;
+import org.mql.jee.trainingcenter.dao.TrainingDaoJdbc;
 
 public class ApplicationContext {
 
@@ -21,6 +25,9 @@ public class ApplicationContext {
 
     private static TrainerService trainerService;
     private static TrainerDao trainerDao;
+
+    private static TrainingService trainingService;
+    private static TrainingDao trainingDao;
 
     static {
 
@@ -33,15 +40,20 @@ public class ApplicationContext {
 
         Database db = new Database(ds);
 
-        // Student
+        // STUDENT
         studentDao = new StudentDaoJdbc(db);
         studentService = new StudentServiceDefault(studentDao);
 
-        // Trainer
+        // TRAINER
         trainerDao = new TrainerDaoJdbc(db);
         trainerService = new TrainerServiceDefault(trainerDao);
+
+        // TRAINING
+        trainingDao = new TrainingDaoJdbc(db);
+        trainingService = new TrainingServiceDefault(trainingDao);
     }
 
+    // STUDENT
     public static StudentService getStudentService() {
         return studentService;
     }
@@ -50,11 +62,21 @@ public class ApplicationContext {
         return studentDao;
     }
 
+    // TRAINER
     public static TrainerService getTrainerService() {
         return trainerService;
     }
 
     public static TrainerDao getTrainerDao() {
         return trainerDao;
+    }
+
+    // TRAINING
+    public static TrainingService getTrainingService() {
+        return trainingService;
+    }
+
+    public static TrainingDao getTrainingDao() {
+        return trainingDao;
     }
 }
