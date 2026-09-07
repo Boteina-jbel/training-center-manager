1e3628240b7115af08d72e93d5582cfdb35d474c
Boteina <boteinajbel@gmail.com>
2026-09-07 22:06:18 +0100

    feat(exception): add EnrollmentException

---
+++ src/main/java/org/mql/jee/trainingcenter/exceptions/EnrollmentException.java
@@ -0,0 +1,10 @@
+package org.mql.jee.trainingcenter.exceptions;
+
+public class EnrollmentException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public EnrollmentException(String message) {
+        super(message);
+    }
+}
